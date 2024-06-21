1edb02afc5c3b26b839c4e280837b921a8dffe41
Nitin-1Fin-0205 <145532168+Nitin-1Fin-0205@users.noreply.github.com>
2024-06-21 21:50:19 +0530

    initial commit

---
+++ cypress/e2e/checkspec.cy.js
@@ -0,0 +1,29 @@
+describe('template spec', () => {
+
+  beforeEach('Exception Handling', () => {
+
+    cy.on('uncaught:exception', (err) => {
+
+      console.log('Exception handling', err);
+      return false;
+
+    });
+
+    cy.visit('https://erp.atriina.com/login');
+
+  });
+
+  it('Login in ERP for checkin', () => {
+
+    cy.get('#login_email').type(Cypress.env('USERNAME'));
+    cy.get('#login_password').type(Cypress.env('PASSWORD'));
+    cy.get('[type="submit"]').contains('Login').click();
+    cy.get('#navbar-search').type('Employee Check');
+    cy.get('#awesomplete_list_1').children().first().click();
+    cy.get('[data-label="Add Employee Checkin"]').click();
+    cy.get('.input-with-feedback.form-control.bold[data-fieldname="employee"]')
+      .type(Cypress.env('EMP_ID'));
+    cy.get('select[data-fieldtype="Select"]').select('IN');
+    // cy.get('[data-label="Save"]').click({ timeout: 10000 });
+  });
+})
