d748ef1ffc91fed67fd07393ba6048cbef491b27
Nitin-1Fin-0205 <145532168+Nitin-1Fin-0205@users.noreply.github.com>
2024-06-21 21:52:38 +0530

    project id added

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "judo3m",
+  projectId: "aszk5d",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
