6bc0912b4662742ed7a81f7d72a9d46ce2ca370d
Nitin Gupta <nitingupta1906@gmail.com>
2024-11-13 16:29:44 +0530

    Check

--- server.js
+++ server.js
@@ -8,8 +8,34 @@ app.use(express.json());
 
 app.post('/save-skip-date', (req, res) => {
     const { skipDate } = req.body;
-    fs.writeFileSync('skipDate.json', JSON.stringify({ skipDate }));
+    let skipDates = [];
+    if (fs.existsSync('skipDates.json')) {
+        skipDates = JSON.parse(fs.readFileSync('skipDates.json'));
+    }
+    if (!skipDates.includes(skipDate)) {
+        skipDates.push(skipDate);
+        fs.writeFileSync('skipDates.json', JSON.stringify(skipDates));
+    }
     res.json({ message: 'Skip date saved successfully' });
+});
+
+app.get('/list-skip-dates', (req, res) => {
+    let skipDates = [];
+    if (fs.existsSync('skipDates.json')) {
+        skipDates = JSON.parse(fs.readFileSync('skipDates.json'));
+    }
+    res.json({ skipDates });
+});
+
+app.post('/remove-skip-date', (req, res) => {
+    const { skipDate } = req.body;
+    let skipDates = [];
+    if (fs.existsSync('skipDates.json')) {
+        skipDates = JSON.parse(fs.readFileSync('skipDates.json'));
+    }
+    skipDates = skipDates.filter(date => date !== skipDate);
+    fs.writeFileSync('skipDates.json', JSON.stringify(skipDates));
+    res.json({ message: 'Skip date removed successfully' });
 });
 
 app.listen(port, () => {
