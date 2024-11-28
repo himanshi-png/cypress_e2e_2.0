da3b37210e35bac30fca31d68bcefe4ce0d4eeec
Nitin Gupta <nitingupta1906@gmail.com>
2024-11-28 13:24:55 +0530

    Nitin :: cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const cors = require('cors');
 const port = 3000;
 
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 
