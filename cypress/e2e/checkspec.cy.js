a0186bd32e6a6dcc43716ddb02f971dd4360baf6
Nitin Gupta <nitingupta1906@gmail.com>
2024-11-28 13:51:57 +0530

    Nitin :: cors

--- cypress/e2e/checkspec.cy.js
+++ cypress/e2e/checkspec.cy.js
@@ -1,5 +1,15 @@
 describe('template spec', () => {
 
+  let skipDates = [];
+
+  before(() => {
+    cy.request('https://cypress-e2e-2-0.onrender.com/list-skip-dates')
+      .then((response) => {
+        expect(response.status).to.eq(200);
+        skipDates = response.body.skipDates;
+      });
+  });
+
   beforeEach('Exception Handling', () => {
 
     cy.on('uncaught:exception', (err) => {
@@ -9,26 +19,29 @@ describe('template spec', () => {
 
     });
 
-    cy.visit('https://erp.atriina.co/login');
+    cy.visit('https://erp.atriina.com/login');
 
   });
 
   it('Login in ERP for checkin', () => {
 
-    const today = new Date().toISOString().split('T')[0];
+    const today = '2024-11-14'
+
+    cy.task('log', `Today: ${today}`, skipDates.includes(today));
+    cy.task('log', `skipDates: ${JSON.stringify(skipDates)}`);
 
     if (skipDates.includes(today)) {
       it('Skip tests for today', () => {
-        cy.log('Tests are skipped for today');
+        cy.task('log', `Tests are skipped for today  ${today}`);
       });
     } else {
       it('Run tests', () => {
         cy.get('#login_email').type(Cypress.env('USERNAME'));
         cy.get('#login_password').type(Cypress.env('PASSWORD'));
         cy.get('[type="submit"]').contains('Login').click();
-        cy.get('#navbar-search').type('Employee Check');
-        cy.get('#awesomplete_list_1').children().first().click();
-        cy.get('[data-label="Add Employee Checkin"]').click();
+        // cy.get('#navbar-search').type('Employee Check');
+        // cy.get('#awesomplete_list_1').children().first().click();
+        // cy.get('[data-label="Add Employee Checkin"]').click();
       });
     }
 
