0c45af4330ebdd276a4e1c50139a5f935215388f
Nitin Gupta <75748537+Nitin-0205@users.noreply.github.com>
2024-12-17 11:26:54 +0530

    checkout added

--- cypress/e2e/checkspec.cy.js
+++ cypress/e2e/checkspec.cy.js
@@ -1,15 +1,5 @@
 describe('template spec', () => {
 
-  let skipDates = [];
-
-  before(() => {
-    cy.request('https://cypress-e2e-2-0.onrender.com/list-skip-dates')
-      .then((response) => {
-        expect(response.status).to.eq(200);
-        skipDates = response.body.skipDates;
-      });
-  });
-
   beforeEach('Exception Handling', () => {
 
     cy.on('uncaught:exception', (err) => {
@@ -25,29 +15,18 @@ describe('template spec', () => {
 
   it('Login in ERP for checkin', () => {
 
-    const today = new Date().toISOString().split('T')[0];
-
-    cy.task('log', `Today: ${today}`, skipDates.includes(today));
-    cy.task('log', `skipDates: ${JSON.stringify(skipDates)}`);
-
-    if (skipDates.includes(today)) {
-      it('Skip tests for today', () => {
-        cy.task('log', `Tests are skipped for today  ${today}`);
-      });
-    } else {
-      it('Run tests', () => {
-        cy.get('#login_email').type(Cypress.env('USERNAME'));
-        cy.get('#login_password').type(Cypress.env('PASSWORD'));
-        cy.get('[type="submit"]').contains('Login').click();
-        cy.get('#navbar-search').type('Employee Check');
-        cy.get('#awesomplete_list_1').children().first().click();
-        cy.get('[data-label="Add Employee Checkin"]').click();
-        cy.get('select[data-fieldtype="Select"][data-fieldname="log_type"]').select('IN');
-        cy.wait(1000);
-        cy.get('[data-label="Save"]').click();
-        cy.wait(1000);
-      });
-    }
-
+    cy.get('#login_email').type(Cypress.env('USERNAME'));
+    cy.get('#login_password').type(Cypress.env('PASSWORD'));
+    cy.get('[type="submit"]').contains('Login').click();
+    cy.get('#navbar-search').type('Employee Check');
+    cy.get('#awesomplete_list_1').children().first().click();
+    cy.get('[data-label="Add Employee Checkin"]').click();
+    // cy.get('.input-with-feedback.form-control.bold[data-fieldname="employee"]')
+    //   .type(Cypress.env('EMP_ID'));
+    // cy.get('select[data-fieldtype="Select"]').select('OUT');
+    cy.get('select[data-fieldtype="Select"][data-fieldname="log_type"]').select('IN');
+    cy.wait(1000);
+    cy.get('[data-label="Save"]').click();
+    cy.wait(1000);
   });
 })
